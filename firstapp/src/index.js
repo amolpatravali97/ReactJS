413d7f15c2f4bd992b735b396356275ddf9b8606
Amol Patravali <amol.patravali97@gmail.com>
2020-01-03 01:02:44 +0530

    converting html to jsx

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hello There!</div>;
+    return (
+        <div>
+            <label class="label" for="name">Enter name: </label>
+            <input id="name" type="text" />
+            <button style= {{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+        </div>
+    );
 };
 
 ReactDOM.render(
