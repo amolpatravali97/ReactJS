18e0601b3b38e36eb413834631e0c2a08d8e1074
Amol Patravali <amol.patravali97@gmail.com>
2020-01-03 00:39:24 +0530

    Hello world

---
+++ firstapp/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>Hello There!</div>;
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
