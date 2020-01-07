f676e1317238724028d0d39a9ac69f1215b110ce
Amol Patravali <amol.patravali97@gmail.com>
2020-01-07 23:36:50 +0530

    Get data from function to JSX

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    const getButtonText = () => "Click Me!";
     return (
         <div>
             <label className="label" for="name">Enter name: </label>
             <input id="name" type="text" />
-            <button style= {{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+            <button style= {{ backgroundColor: 'blue', color: 'white' }}>{ getButtonText() }</button>
         </div>
     );
 };
