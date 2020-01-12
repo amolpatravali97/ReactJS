c844b9aa5a30e23d597f32323c21e04e2c0cd673
Amol Patravali <amol.patravali97@gmail.com>
2020-01-12 22:39:03 +0530

    Display season names based on location and date api

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = { lat: null, errorMessage: '' };
+    }
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                console.log(position);
-                this.setState({lat: position.coords.latitude});
-            },
-            (err) => {
-                console.log(err);
-                this.setState({ errorMessage: err.message });
-            }
+            position =>
+                this.setState({lat: position.coords.latitude}),
+            err => this.setState({ errorMessage: err.message })
         );
     }
 
     render() {
         if (this.state.lat && !this.state.errorMessage) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <div><SeasonDisplay lat={this.state.lat} /></div>
         }
         if (!this.state.lat && this.state.errorMessage) {
             return <div>Error: {this.state.errorMessage}</div>
