fd0c3402278028593133430a1a9810380730d674
Amol Patravali <amol.patravali97@gmail.com>
2020-01-12 18:59:33 +0530

    Conditionals

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -16,12 +16,16 @@ class App extends React.Component {
             }
         );
     }
+
     render() {
+        if (this.state.lat && !this.state.errorMessage) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        if (!this.state.lat && this.state.errorMessage) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
         return (
-            <div>
-                Latitude: {this.state.lat}<br />
-                Error: {this.state.errorMessage}
-            </div>
+            <div>Loading!</div>
         );
     }
 }
