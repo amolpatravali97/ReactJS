40d0e3dcb3771d551708ac458283a39a6e7c4c6f
Amol Patravali <amol.patravali97@gmail.com>
2020-01-12 18:50:06 +0530

    States

---
+++ seasons/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { lat: null, errorMessage: '' };
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position);
+                this.setState({lat: position.coords.latitude});
+            },
+            (err) => {
+                console.log(err);
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+    render() {
+        return (
+            <div>
+                Latitude: {this.state.lat}<br />
+                Error: {this.state.errorMessage}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
