d7c4d1bc21718951a5c8c287df9570b7832351b8
Amol Patravali <amol.patravali97@gmail.com>
2020-01-18 23:50:08 +0530

    Changed from functional to class comp.

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
-const SearchBar = () => {
-    return (
-        <div className="ui segment">
-            <form className="ui form">
-                <div className="field">
-                    <label>Image Search</label>
-                    <input type="text" />
-                </div>
-            </form>
-        </div>
-    );
+
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" />
+                    </div>
+                </form>
+            </div>
+        );
+    }
 };
 
 export default SearchBar;
