1d2cc85695ccc8a4ab3818b622155b6951d3601c
Amol Patravali <amol.patravali97@gmail.com>
2020-01-20 22:42:48 +0530

    Alternate way to bind callback function

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 class SearchBar extends React.Component {
     state = { term: '' };
 
-    onFormSubmit(event) {
+    onFormSubmit = event => {
         event.preventDefault();
         console.log(this.state.term);
     }
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div className="ui segment">
-                <form onSubmit={this.onFormSubmit.bind(this)} className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text" value={this.state.term} onChange={ (e) => {this.setState({ term: e.target.value })}} />
