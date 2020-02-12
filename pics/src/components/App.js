e912e5d08292bf553339c8dbef44a16bcc0d6c8f
Amol Patravali <amol.patravali97@gmail.com>
2020-02-12 23:50:30 +0530

    Api to request images

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import axios from 'axios';
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
-    onSearchSubmit(e) {
-        console.log(e);
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 36c58bf0e1c8303fa773c53ad577e5a424e4517e292fc7c394afc3ad76643cd7'
+            }
+        });
+        
+        console.log('Result\n', response.data.results);
     }
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={this.onSubmit}/>
+                <SearchBar onSubmit={this.onSearchSubmit} />
             </div>
         );
     }
