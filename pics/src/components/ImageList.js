1300cd61066f422f8b937554bff609e732d47044
Amol Patravali <amol.patravali97@gmail.com>
2020-02-23 23:34:31 +0530

    Added key prop to list element, alt

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,8 +1,7 @@
 import React from 'react'
 
 const ImageList = (props) => {
-    console.log(props.images);
-    const images = props.images.map(image => <img src={image.urls.regular} alt=""/>);
+    const images = props.images.map(({ description, id, urls}) => <img alt={description} key={id} src={urls.regular}/>);
     return (
         <div>{images}</div>
     );
