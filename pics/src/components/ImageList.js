cec27e1cb58a3b765bb93802214da3f48d117120
Amol Patravali <amol.patravali97@gmail.com>
2020-02-29 16:09:13 +0530

    Styling of images in grid layout

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,10 @@
+import './ImageList.css'
 import React from 'react'
 
 const ImageList = (props) => {
     const images = props.images.map(({ description, id, urls}) => <img alt={description} key={id} src={urls.regular}/>);
     return (
-        <div>{images}</div>
+        <div className="image-list">{images}</div>
     );
 };
 
