f8725922e1822655d3450b549d4eac0103c0aae0
Amol Patravali <amol.patravali97@gmail.com>
2020-03-19 23:59:48 +0530

    Rendering each image according to height

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -4,18 +4,22 @@ class ImageCard extends React.Component {
     constructor(props) {
         super(props);
         this.imageRef = React.createRef();
-        
+        this.state = { span: 0 };
     }
     componentDidMount() {
         this.imageRef.current.addEventListener('load', this.setSpans);
     }
     setSpans = () => {
-        console.log(this.imageRef.current.clientHeight);
+        const height = this.imageRef.current.clientHeight;
+        const span = Math.ceil(height/10);
+
+        this.setState({ span });
     }
     render() {
         const {description, urls} = this.props.image;
+
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.span}` }}>
                 <img ref={this.imageRef} src={urls.regular} alt={description} />
             </div>
         );
